f334aaa81e186eb41db6f03577e814b6545da437
Sergey <owlbert@null.net>
2022-07-11 14:18:40 +0700

    init app

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
+
+
+const app = createApp(App).use(Quasar, quasarUserOptions)
+
+app.use(router)
+
+app.mount('#app')
