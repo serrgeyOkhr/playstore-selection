51550d78d082b9a4f1a98806152c6856e4e99ca2
Owlbert <owlbert@null.net>
2022-07-15 09:05:51 +0700

    New sort. New search select

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -2,38 +2,70 @@
   <div class="q-pa-md justify-center stretch q-gutter-md">
     <div> Main Title</div>
     <div  class="control_panel">
-      <div class="range_container">
-        <span>Select Price</span>
-        <q-range
-          v-model="searchSettings.priceRange"
-          :min="0"
-          :max="50"
-          :step="0.5"
-          label
-        />
+      <div class="selectorWithParams">
+        <div class="sliders_container">
+          <div class="range_container">
+            <span>Select Price</span>
+            <q-range
+              v-model="searchSettings.priceRange"
+              :min="0"
+              :max="50"
+              :step="0.5"
+              label
+            />
+          </div>
+          <div class="range_container">
+            <span>Select min Steam rating</span>
+            <q-slider
+              v-model="searchSettings.steamRating"
+              :min="0"
+              :max="100"
+              :step="0.5"
+              label
+            />
+          </div>
+          <div class="range_container">
+            <span>Select min Metacritic rating</span>
+            <q-slider
+              v-model="searchSettings.metacriticRating"
+              :min="0"
+              :max="100"
+              :step="0.5"
+              label
+            />
+          </div>
+        </div>
+        <q-checkbox left-label v-model="searchSettings.onSale" label="On Sale" />
+        <q-btn @click="searchGames()">Find!</q-btn>
       </div>
-      <div class="range_container">
-        <span>Select Steam rating</span>
-        <q-slider
-          v-model="searchSettings.steamRating"
-          :min="0"
-          :max="100"
-          :step="0.5"
-          label
-        />
+      <div class="shopSelector">
+        <q-checkbox left-label v-model="isShowShopSelect" label="Choose shop for select" />
+        <div class="shop_container" v-if="isShowShopSelect">
+          <div class="shop_item border rounded-borders q-ma-xs" v-for="(store, index) in stores" :key="index">
+          <q-item tag="label" v-ripple>
+            <q-item-section  side center>
+              <q-checkbox v-model="showFromShop" :val="index+1" />
+            </q-item-section>
+            <q-item-section>
+              <q-img
+              class="q-mb-xs"
+              :src="'https://www.cheapshark.com/'+store.images.logo"
+              fit="contain"
+              width="60%"
+              />
+              <q-item-label>{{ store.storeName }}</q-item-label>
+              <q-item-label caption>
+                Show games from this store?
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+            <div>
+              
+              </div>
+          </div>
+        </div>
+              <!-- <q-btn @click="searchGames()">Apply</q-btn> -->
       </div>
-      <div class="range_container">
-        <span>Select Metacritic rating</span>
-        <q-slider
-          v-model="searchSettings.metacriticRating"
-          :min="0"
-          :max="100"
-          :step="0.5"
-          label
-        />
-      </div>
-      <q-checkbox left-label v-model="searchSettings.onSale" label="On Sale" />
-      <q-btn @click="searchGames()">Find!</q-btn>
     </div>
     <div class="pagination">
       <div class="q-pa-lg flex flex-center">
@@ -45,20 +77,12 @@
           direction-links
         />
         <div class="sortBy">
-          <q-btn-toggle
+          Sort by: 
+          <q-select
             v-model="searchSettings.sortBy"
-            @click="searchGames"
+            @update:model-value="searchGames()"
             toggle-color="primary"
-            :options="[
-              {label: 'Default', value: 'Deal Rating'},
-              {label: 'Title', value: 'Title'},
-              {label: 'Savings', value: 'Savings'},
-              {label: 'Price', value: 'Price'},
-              {label: 'Metacritic', value: 'Metacritic'},
-              {label: 'Reviews', value: 'Reviews'},
-              {label: 'Release', value: 'Release'},
-              {label: 'Store', value: 'Store'},
-            ]"
+            :options="sortByOptions"
           />
         </div>
       </div>
@@ -111,10 +135,22 @@ export default{
       sortBy: 'Deal Rating'
 
     })
+    const sortByOptions = [
+              {label: 'Default', value: 'Deal Rating'},
+              {label: 'Title', value: 'Title'},
+              {label: 'Savings', value: 'Savings'},
+              {label: 'Price', value: 'Price'},
+              {label: 'Metacritic', value: 'Metacritic'},
+              {label: 'Reviews', value: 'Reviews'},
+              {label: 'Release', value: 'Release'},
+              {label: 'Store', value: 'Store'},
+            ]
+    const isShowShopSelect =ref(false)
     const showGames = ref([])
     const stores = ref([])
     const maxPages = ref(0)
     const currentPage = ref(1)
+    const showFromShop = ref([])
     const API_Section = {
       List_of_Deals: 'deals?',
       Deal_Lookup: 'deals?',
@@ -148,7 +184,8 @@ export default{
         upperPrice: searchSettings.value.priceRange.max,
         metacritic: searchSettings.value.metacriticRating,
         steamRating: searchSettings.value.steamRating,
-        sortBy: searchSettings.value.sortBy
+        sortBy: searchSettings.value.sortBy.value,
+        storeID: (showFromShop.value.length > 0) ? showFromShop.value.toString() : undefined
       }
       const fetch_options = {
         method: 'GET',
@@ -201,6 +238,7 @@ export default{
       .then((response) => {
         maxPages.value = Number(response.headers.get('X-Total-Page-Count'));
         console.log('maxPages = ', maxPages.value);
+        // response.headers.forEach(header => console.log(header))
         return response.json()
         })
       .then((result) => {
@@ -215,7 +253,12 @@ export default{
     function searchGames() {
       searchWithParams(showGames, maxPages, currentPage)
     }
+    function changeValue() {
+      console.log('showFromShop', showFromShop.value);
+      console.log('showFromShop toString = ', showFromShop.value.toString());
 
+
+    }
 
     return {
       showGames,
@@ -223,16 +266,27 @@ export default{
       maxPages,
       currentPage,
       searchSettings,
+      showFromShop,
+      isShowShopSelect,
+      sortByOptions,
       getGames,
-      searchGames
+      searchGames,
+      changeValue
     }
   }
 }
 </script>
 
 <style scoped>
-.card_wrapper, .range_container {
+.card_wrapper{
   width: 20%;
+}
+.sliders_container{
+  display: flex;
+}
+.range_container{
+  width: 30%;
+  margin-left: 20px;
 }
 .card_container {
   width: 100%;
@@ -242,5 +296,17 @@ export default{
 .preloader {
   display: flex;
   /* position: absolute */
+}
+.shop_container{
+  display: flex;
+  flex-wrap: wrap;
+}
+.shop_item{
+  display: flex;
+  width: 12%;
+}
+.sortBy{
+  display: flex;
+  align-items: center;
 }
 </style>
