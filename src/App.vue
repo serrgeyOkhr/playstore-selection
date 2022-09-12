91620c87b235d71d23eca236f6bac6acab9ff155
Sergey <owlbert@null.net>
2022-09-12 20:13:32 +0700

    new game page style

--- src/App.vue
+++ src/App.vue
@@ -16,5 +16,22 @@
   display: flex;
   flex-direction: row;
 }
-
+.bigDescription h1 {
+    font-size: 26px;
+    margin: 0;
+    line-height: normal;
+  }
+  .bigDescription h2 {
+    font-size: 24px;
+    font-weight: bold;
+    margin-top: 10px;
+    margin-bottom: 5px;
+    line-height: normal;
+  }
+  .bigDescription p {
+    margin: 0px;
+  }
+  .bigDescription ul {
+    margin-top: 0px;
+  }
 </style>
--- src/pages/ItemView.vue
+++ src/pages/ItemView.vue
@@ -16,142 +16,132 @@
       </q-toolbar>
     </q-header>
     <q-page-container>
-      <q-page padding>
-        <div class="container">
-          <div class="itemTitle flex">
-            <div class="gameImgs q-pr-lg">
-              <div>
-                <div class="q-mb-md">
+      <q-page>
+        <div
+          class="page"
+          :style="{ 'background-image': 'url(' + steamInfo?.background + ')' }"
+        >
+          <div class="container">
+            <div class="gameTitle q-mb-md">
+              <span class="text-h3">{{ steamInfo?.name }}</span>
+            </div>
+            <div class="presentGame">
+              <div class="gameImgs q-pr-lg">
+                <q-carousel
+                  swipeable
+                  animated
+                  arrows
+                  v-model="slide"
+                  thumbnails
+                  infinite
+                  navigation
+                  :height="'350px'"
+                >
+                  <q-carousel-slide
+                    class="carouselSlide"
+                    v-for="(img, index) in steamInfo?.screenshots"
+                    :key="index"
+                    :name="index"
+                    :img-src="img.path_thumbnail"
+                  />
+                </q-carousel>
+              </div>
+              <div class="gameDescription">
+                <div class="q-mb-md header_img">
                   <q-img
                     fit
                     :src="steamInfo?.header_image"
                   />
                 </div>
+                <div class="gameShortDesc">
+                  {{ steamInfo?.short_description }}
+                </div>
                 <div>
-                  <q-carousel
-                    swipeable
-                    animated
-                    arrows
-                    v-model="slide"
-                    thumbnails
-                    infinite
-                    navigation
+                  <span class="text-h5"
+                    >Lowest price ever:
+                    {{ gameInfo?.cheapestPriceEver.price }} $</span
                   >
-                    <q-carousel-slide
-                      v-for="(img, index) in steamInfo?.screenshots"
-                      :key="index"
-                      :name="index"
-                      :img-src="img.path_thumbnail"
-                    />
-                  </q-carousel>
                 </div>
               </div>
-              <!-- <div class="historical_block">
-                <p class="text-h5">Lowest price ever:</p>
-                <span class="text-body1"> {{gameInfo?.cheapestPriceEver.price}} </span>
-                <span class="text-body1"> {{gameInfo?.cheapestPriceEver.date}} </span>
-              </div> -->
             </div>
-            <div class="gameInfo flex">
-              <div class="q-mb-md">
-                <span class="text-h3">{{ gameInfo?.info.title }}</span>
-              </div>
-              <div>
-                <span class="text-body1">
-                  {{ steamInfo?.short_description }}
-                </span>
-                <p class="text-h5">
-                  Lowest price ever: {{ gameInfo?.cheapestPriceEver.price }} $
-                </p>
+            <!-- <div class="itemTitle flex"> -->
+            <!-- <pre> {{ steamInfo }} </pre> -->
+            <div class="gameInfo">
+              <div class="bigDescription">
+                <h2>About</h2>
+                <span
+                  class="text-body1"
+                  v-html="steamInfo?.about_the_game"
+                ></span>
                 <span class="text-body1"> </span>
               </div>
 
               <!-- <button @click="checkConnection">Проверить соединение</button> -->
+            </div>
+            <div class="price_place">
+              <span class="text-h5"> Список всех магазинов: </span>
+              <div class="card_container">
+                <q-card
+                  class="my-card q-ma-md"
+                  v-for="(game, index) in gameInfo?.deals"
+                  :key="index"
+                >
+                  <!-- <q-img
+                    fit="contain"
+                    height="150px"
+                    :src="game.thumb"
+                    /> -->
 
-              <div class="price_place q-mt-md">
-                <span class="text-h5"> Список всех магазинов: </span>
-                <div class="card_container">
-                  <q-card
-                    class="my-card q-ma-md"
-                    v-for="(game, index) in gameInfo?.deals"
-                    :key="index"
-                  >
-                    <!-- <q-img
-                      fit="contain"
-                      height="150px"
-                      :src="game.thumb"
-                      /> -->
-
-                    <q-card-section>
-                      <!-- <div class="text-overline text-orange-9">Overline</div> -->
-                      <!-- <div class="text-h5 q-mt-sm q-mb-xs">{{game.title}}</div> -->
-                      <div class="descriptionContainer text-caption q-mb-md">
-                        <div class="storeImgContainer">
-                          <q-img
-                            :src="
-                              'https://www.cheapshark.com/' +
-                              stores.filter(
-                                (shop) => game.storeID === shop.storeID
-                              )[0]?.images.logo
-                            "
-                            alt=""
-                          />
-                        </div>
-                        <div>
-                          <span
-                            v-if="game.retailPrice - game.price != 0"
-                            class="text-red-5"
-                          >
-                            Распродажа! </span
-                          ><br />
-                          <span class="text-body1"
-                            >В магазине
-                            {{
-                              stores.filter(
-                                (shop) => game.storeID === shop.storeID
-                              )[0]?.storeName
-                            }}</span
-                          >
-                        </div>
+                  <div class="q-pa-md">
+                    <div class="descriptionContainer text-caption q-mb-md">
+                      <div class="storeImgContainer">
+                        <q-img
+                          :src="
+                            'https://www.cheapshark.com/' +
+                            stores.filter(
+                              (shop) => game.storeID === shop.storeID
+                            )[0]?.images.logo
+                          "
+                          alt=""
+                        />
                       </div>
-                      <div class="price">
-                        Цена:
+                      <div>
                         <span
                           v-if="game.retailPrice - game.price != 0"
-                          class="normalPrice q-mr-sm text-strike"
-                          >{{ game.retailPrice }}$</span
+                          class="text-red-5"
                         >
-                        <span class="salePrice text-h5 text-red-7"
-                          >{{ game.price }}$</span
+                          Распродажа! </span
+                        ><br />
+                        <span class="text-body1"
+                          >В магазине
+                          {{
+                            stores.filter(
+                              (shop) => game.storeID === shop.storeID
+                            )[0]?.storeName
+                          }}</span
                         >
                       </div>
-                    </q-card-section>
-
-                    <!-- <q-card-section class="q-pt-none">
-                        <div class="text-subtitle1">
-                          $・Italian, Cafe
-                        </div>
-                        <div class="text-caption text-grey">
-                          Small plates, salads & sandwiches in an intimate setting.
-                        </div>
-                      </q-card-section> -->
-
-                    <q-separator />
-
-                    <!-- <q-card-actions align="right">
-                        <q-btn v-close-popup flat color="primary" label="Reserve" />
-                        <q-btn v-close-popup flat color="primary" round icon="event" />
-                      </q-card-actions> -->
-                  </q-card>
-                </div>
+                    </div>
+                    <div class="price">
+                      Цена:
+                      <span
+                        v-if="game.retailPrice - game.price != 0"
+                        class="normalPrice q-mr-sm text-strike"
+                        >{{ game.retailPrice }}$</span
+                      >
+                      <span class="salePrice text-h5 text-red-7"
+                        >{{ game.price }}$</span
+                      >
+                    </div>
+                  </div>
+                  <q-separator />
+                </q-card>
               </div>
             </div>
           </div>
-          <!-- <q-img :src="steamInfo?.background"></q-img> -->
         </div>
-        <!-- <pre>{{steamInfo}}</pre> -->
-        <!-- <pre>{{gameInfo}}</pre> -->
-        <!-- <pre>{{stores}}</pre> -->
+        <!-- <q-img :src="steamInfo?.background"></q-img> -->
+        <!-- </div> -->
       </q-page>
     </q-page-container>
   </q-layout>
@@ -171,6 +161,7 @@
       const gameID = ref(0);
       const steamID = ref(0);
       const stores = ref([]);
+      const showMore = ref('больше');
 
       getStores(stores);
       getGameID(route, gameID);
@@ -188,35 +179,19 @@
         output.value = Number(route.params.id);
       }
 
-      // async function getSteamInfo(gameID, output) {
-      //   const gameInfoURL = `https://store.steampowered.com/api/appdetails?appids=${gameID.value}`;
-      //   // console.log(gameInfoURL);
-      //   const params = {
-      //     method: 'GET',
-      //     headers: {
-      //       'Access-Control-Allow-Origin':
-      //         'https://calm-refuge-07361.herokuapp.com',
-      //       // 'Content-Type':	'application/json;',
-      //     },
-      //     mode: 'cors',
-      //   };
-      //   fetch(gameInfoURL, params)
-      //     .then((response) => {
-      //       console.log(response);
-      //       return response.json();
-      //     })
-      //     .then((result) => {
-      //       console.log('gameInfo', result);
-      //       output.value = result[gameID.value].data;
-      //     });
-      //   // await sendOnServer(gameInfoURL, params, output)
-      // }
+      function changeDescription() {
+        if (showMore.value === 'больше') {
+          showMore.value = 'меньше';
+        } else {
+          showMore.value = 'больше';
+        }
+      }
 
       async function getLocalSteamInfo(gameID, output) {
         const gameInfoURL = `https://server-for-cinema-app.herokuapp.com/api/getSteamInfo?appids=${gameID.value}`;
         // console.log(gameInfoURL);
         let myHeaders = new Headers({
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         });
         const params = {
           method: 'GET',
@@ -227,7 +202,7 @@
           .then((response) => response.json())
           .then((result) => {
             console.log('got from my server', result);
-            output.value = result[gameID.value].data
+            output.value = result[gameID.value].data;
           });
         // await sendOnServer(gameInfoURL, params, output)
       }
@@ -235,7 +210,7 @@
       function checkConnection() {
         const gameInfoURL = `http://localhost:5000/api/getSteamInfo?appids=8870`;
         let myHeaders = new Headers({
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         });
         const params = {
           method: 'GET',
@@ -292,21 +267,64 @@
         slide,
         stores,
         steamInfo,
-        checkConnection
+        checkConnection,
+        changeDescription,
+        showMore,
       };
     },
   };
 </script>
 
-<style>
+<style scoped>
+  .page {
+    background-repeat: no-repeat;
+    background-size: cover;
+    min-height: calc(100vh - 50px);
+    padding: 20px;
+  }
+  .container {
+    width: 1140px;
+    margin: 0 auto;
+    background-color: rgb(240, 240, 240);
+    padding: 15px;
+    border-radius: 15px;
+  }
+  .presentGame {
+    display: flex;
+    background-color: #ccc;
+    padding: 15px;
+    /* border: 2px solid rgb(250, 250, 250); */
+    border-top-left-radius: 30px;
+    border-top-right-radius: 30px;
+    overflow: hidden;
+  }
+  .price_place {
+    background-color: #ccc;
+    padding: 15px;
+    /* border: 2px solid rgb(250, 250, 250); */
+    border-bottom-left-radius: 30px;
+    border-bottom-right-radius: 30px;
+  }
+
   .gameImgs {
+    width: 60%;
+    border-top-left-radius: 15px;
+    overflow: hidden;
+  }
+  .gameDescription {
     width: 40%;
     display: flex;
     flex-direction: column;
+    border-top-right-radius: 15px;
+    overflow: hidden;
   }
   .itemTitle {
     display: flex;
     width: 100%;
+  }
+  .gameTitle {
+    display: flex;
+    justify-content: center;
   }
   .my-card {
     width: 45%;
@@ -315,6 +333,7 @@
     display: flex;
     flex-wrap: wrap;
     flex-direction: row;
+    justify-content: center;
   }
   .descriptionContainer {
     display: flex;
@@ -325,7 +344,23 @@
     margin-right: 25px;
   }
   .gameInfo {
+    display: flex;
     flex-direction: column;
-    width: 60%;
+    padding: 10px 15px 10px 15px;
+    background-color: rgb(236, 234, 234);
+  }
+  .link_alike {
+    border: none;
+    background-color: transparent;
+    text-decoration: underline;
+    color: #027be3;
+  }
+  .link_alike:hover {
+    cursor: pointer;
+    text-decoration: underline;
+  }
+  .carouselSlide {
+    background-size: contain;
+    background-repeat: no-repeat;
   }
 </style>
